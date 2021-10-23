32a591f550873312737c286f277be0c9893c5ec0
Jackie Xu <jackie@theflokiinu.com>
2021-10-23 14:08:31 +0200

    chore: initial project setup

---
+++ test/types.ts
@@ -0,0 +1,14 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { Fixture } from "ethereum-waffle";
+
+
+declare module "mocha" {
+  export interface Context {
+    loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
+    signers: Signers;
+  }
+}
+
+export interface Signers {
+  admin: SignerWithAddress;
+}
