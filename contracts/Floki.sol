e7c6778fb8bb8b41d1fa156ac03e55b4b4ea633b
Jackie Xu <jackie@theflokiinu.com>
2021-11-12 17:26:13 +0100

    fix: resolve unbounded compiler issue (FCK-01)

--- contracts/Floki.sol
+++ contracts/Floki.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicensed
-pragma solidity ^0.8.4;
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
