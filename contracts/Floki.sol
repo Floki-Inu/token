8a2b902b5c2f466d16bd11b6cbd5d471fe77ad87
Jackie Xu <jackie@theflokiinu.com>
2021-11-12 17:35:45 +0100

    chore: remove unused deliver function (FCK-09)

--- contracts/Floki.sol
+++ contracts/Floki.sol
@@ -162,15 +162,6 @@ contract FLOKI is Context, IERC20, Ownable {
         return _tFeeTotal;
     }
 
-    function deliver(uint256 tAmount) public {
-        address sender = _msgSender();
-        require(!_isExcluded[sender], "Excluded addresses cannot call this function");
-        (uint256 rAmount, , , , , ) = _getValues(tAmount);
-        _rOwned[sender] = _rOwned[sender].sub(rAmount);
-        _rTotal = _rTotal.sub(rAmount);
-        _tFeeTotal = _tFeeTotal.add(tAmount);
-    }
-
     function reflectionFromToken(uint256 tAmount, bool deductTransferFee) public view returns (uint256) {
         require(tAmount <= _tTotal, "Amount must be less than supply");
         if (!deductTransferFee) {
