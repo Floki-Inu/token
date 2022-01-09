435cbb8096d8c7fd7dc7181be486f32a73b31e8a
Jackie Xu <jackie@theflokiinu.com>
2022-01-09 15:18:44 +0100

    chore: add verification to deploy

--- scripts/deploy-floki.ts
+++ scripts/deploy-floki.ts
@@ -14,7 +14,7 @@ import {
 } from "../types";
 
 async function main(): Promise<void> {
-  const name = "NHAM-Test-01";
+  const name = "NHAM-Test-02";
   const routerAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
   const signers = await hre.ethers.getSigners();
@@ -32,7 +32,7 @@ async function main(): Promise<void> {
   const zeroTreasuryHandler: ZeroTreasuryHandler = await ZeroTreasuryHandler.deploy();
 
   const floki: FLOKI = await Floki.deploy(name, name, zeroTaxHandler.address, zeroTreasuryHandler.address);
-  const staticTaxHandler: StaticTaxHandler = await StaticTaxHandler.deploy(floki.address);
+  const staticTaxHandler: StaticTaxHandler = await StaticTaxHandler.deploy();
   const treasuryHandlerAlpha: TreasuryHandlerAlpha = await TreasuryHandlerAlpha.deploy(
     signer.address,
     floki.address,
@@ -41,6 +41,54 @@ async function main(): Promise<void> {
 
   await floki.setTaxHandler(staticTaxHandler.address);
   await floki.setTreasuryHandler(treasuryHandlerAlpha.address);
+
+  // Sleep for 5 minutes to give the network enough time.
+  await new Promise(resolve => setTimeout(resolve, 5000));
+
+  try {
+    await hre.run("verify:verify", {
+      address: zeroTaxHandler.address,
+      constructorArguments: [],
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    await hre.run("verify:verify", {
+      address: zeroTreasuryHandler.address,
+      constructorArguments: [],
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    await hre.run("verify:verify", {
+      address: floki.address,
+      constructorArguments: [name, name, zeroTaxHandler.address, zeroTreasuryHandler.address],
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    await hre.run("verify:verify", {
+      address: staticTaxHandler.address,
+      constructorArguments: [],
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    await hre.run("verify:verify", {
+      address: treasuryHandlerAlpha.address,
+      constructorArguments: [signer.address, floki.address, routerAddress],
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere and properly handle errors.
