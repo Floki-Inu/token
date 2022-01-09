979cf4524e20ddbfc3cee1133dd759fbaab6a01a
Jackie Xu <jackie@theflokiinu.com>
2022-01-09 23:02:58 +0100

    chore: add docs and events to exchange pool processor

--- contracts/utils/ExchangePoolProcessor.sol
+++ contracts/utils/ExchangePoolProcessor.sol
@@ -4,20 +4,54 @@ pragma solidity 0.8.11;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
+/**
+ * @title Exchange pool processor abstract contract.
+ * @dev Keeps an enumerable set of designated exchange addresses as well as a single primary pool address.
+ */
 abstract contract ExchangePoolProcessor is Ownable {
     using EnumerableSet for EnumerableSet.AddressSet;
 
     EnumerableSet.AddressSet internal _exchangePools;
+
+    /// @dev Primary exchange pool address.
     address public primaryPool;
 
+    /// @dev Emitted when an exchange pool address is added to the set of tracked pool addresses.
+    event ExchangePoolAdded(address exchangePool);
+
+    /// @dev Emitted when an exchange pool address is removed from the set of tracked pool addresses.
+    event ExchangePoolRemoved(address exchangePool);
+
+    /// @dev Emitted when the primary pool address is updated.
+    event PrimaryPoolUpdated(address oldPrimaryPool, address newPrimaryPool);
+
+    /**
+     * @notice Add an address to the set of exchange pool addresses.
+     * @dev Nothing happens if the pool already exists in the set.
+     * @param exchangePool Address of exchange pool to add.
+     */
     function addExchangePool(address exchangePool) external onlyOwner {
-        _exchangePools.add(exchangePool);
+        if (_exchangePools.add(exchangePool)) {
+            emit ExchangePoolAdded(exchangePool);
+        }
     }
 
+    /**
+     * @notice Remove an address from the set of exchange pool addresses.
+     * @dev Nothing happens if the pool doesn't exist in the set..
+     * @param exchangePool Address of exchange pool to remove.
+     */
     function removeExchangePool(address exchangePool) external onlyOwner {
-        _exchangePools.remove(exchangePool);
+        if (_exchangePools.remove(exchangePool)) {
+            emit ExchangePoolRemoved(exchangePool);
+        }
     }
 
+    /**
+     * @notice Set exchange pool address as primary pool.
+     * @dev To prevent issues, only addresses inside the set of exchange pool addresses can be selected as primary pool.
+     * @param exchangePool Address of exchange pool to set as primary pool.
+     */
     function setPrimaryPool(address exchangePool) external onlyOwner {
         require(
             _exchangePools.contains(exchangePool),
@@ -28,6 +62,9 @@ abstract contract ExchangePoolProcessor is Ownable {
             "ExchangePoolProcessor:setPrimaryPool:ALREADY_SET - This address is already the primary pool address."
         );
 
+        address oldPrimaryPool = primaryPool;
         primaryPool = exchangePool;
+
+        emit PrimaryPoolUpdated(oldPrimaryPool, exchangePool);
     }
 }
